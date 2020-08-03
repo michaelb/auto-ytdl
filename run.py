06e65e31c710771d79d21c45d37b590626624e50
michaelb <michael.bleuez2@gmail.com>
2020-08-03 10:12:30 +0200

    pyinstaller structure

---
+++ run.py
@@ -0,0 +1,14 @@
+import packaging.version
+import packaging.specifiers
+import packaging.requirements
+import packaging.markers
+
+from autoytdl.AYTDL import main
+
+if __name__ == '__main__':
+    main()
+
+
+# this is a launcher but also a build tool for pyinstaller
+# It is used as a memo and to help different contributors package the application in the same way
+# On (arch) linux, I compile with "pytinstaller run.py --onefile"
