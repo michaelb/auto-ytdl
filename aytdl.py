40ebd407edf093e6c4086352ec4855fa126b3520
michaelb <michael.bleuez2@gmail.com>
2020-08-04 11:36:35 +0200

    fix typo in aytdl

--- aytdl.py
+++ aytdl.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
 # this is a launcher but also a build tool for pyinstaller
 # It is used as a memo and to help different contributors package the application in the same way
 # On (arch) linux, I compile with:
-# pyinstaller run.py --onedir --exclude scipy --exclude tk --exclude numpy --exclude PySide2 --exclude shiboken2 --exclude Pillow-PIL
+# pyinstaller aytdl.py --onedir --exclude scipy --exclude tk --exclude numpy --exclude PySide2 --exclude shiboken2 --exclude Pillow-PIL
